863e85b65b0756fec5b9584f8150893b8676ba41
Aser Crespo <aser.crespo@fundacionctic.org>
2022-09-27 21:29:48 +0200

    Initial slides application.

---
+++ slides/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Slides from './lib/Slides.svelte';
+  import slideContent from './assets/ctic-con-slides.md?url'
+</script>
+
+
+<Slides markdown_content={slideContent}/>
+
+
+
---
+++ slides/src/lib/Slides.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Reveal from 'reveal.js'
+    import Markdown from 'reveal.js/plugin/markdown/markdown.js'
+    import { onMount } from 'svelte';
+    
+    export let markdown_content;
+
+    let deck_canvas;
+
+    onMount(() => {
+        let deck = Reveal(deck_canvas, {
+            plugins: [ Markdown ]
+        });
+        deck.initialize();
+    });
+</script>
+
+<div class="reveal" bind:this={deck_canvas}>
+    <div class="slides">
+        <section data-markdown={markdown_content}>
+        </section>
+    </div>
+</div>
