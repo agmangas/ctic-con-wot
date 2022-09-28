3ee2fe1f4aec13b4d594c6ef029a2ff194896e1d
Aser Crespo <aser.crespo@fundacionctic.org>
2022-09-28 22:53:00 +0200

    Basic presentation on HTML instead of MD.

--- slides/src/App.svelte
+++ slides/src/App.svelte
@@ -1,10 +1,9 @@
 <script>
   import Slides from './lib/Slides.svelte';
-  import slideContent from './assets/ctic-con-slides.md?url'
 </script>
 
 
-<Slides markdown_content={slideContent}/>
+<Slides />
 
 
 
--- slides/src/lib/Slides.svelte
+++ slides/src/lib/Slides.svelte
@@ -1,23 +1,103 @@
 <script>
-    import Reveal from 'reveal.js'
-    import Markdown from 'reveal.js/plugin/markdown/markdown.js'
-    import { onMount } from 'svelte';
-    
-    export let markdown_content;
+  import Reveal from "reveal.js";
+  import { onMount } from "svelte";
 
-    let deck_canvas;
+  let deck_canvas;
 
-    onMount(() => {
-        let deck = Reveal(deck_canvas, {
-            plugins: [ Markdown ]
-        });
-        deck.initialize();
+  onMount(() => {
+    let deck = Reveal(deck_canvas, {
+      minScale: 0.2,
     });
+    deck.initialize();
+  });
 </script>
 
 <div class="reveal" bind:this={deck_canvas}>
-    <div class="slides">
-        <section data-markdown={markdown_content}>
-        </section>
-    </div>
+  <div class="slides">
+    <section>
+      <h1>WoT</h1>
+      <h2>Web of Things</h2>
+      <span class="fragment">
+        <p>es... como Internet de las Cosas</p>
+      </span>
+      <p class="fragment">(pero mejor)</p>
+    </section>
+    <section>
+      <h2>IoT</h2>
+      <p>Dispositivos que se comunican con ordenadores y entre sí</p>
+      <p>Ordenadores que se comunican entre sí</p>
+      <p>Servicios para usar los datos obtenidos</p>
+    </section>
+    <section>
+      <h2>¿Y WoT?</h2>
+      <p>Estándar para saber cómo se tienen que comunicar entre sí</p>
+    </section>
+    <section>
+      <div class="r-fit-text">
+        <table style="width: 100%">
+          <tr>
+            <th>Andrés</th>
+            <th>Santi</th>
+            <th>Dani</th>
+            <th>Aser</th>
+            <th>Javi</th>
+            <th>Sergio</th>
+          </tr>
+          <tr>
+            <td>Arquitecturas</td>
+            <td>Cacharrear</td>
+            <td>Microcontroladores</td>
+            <td>Lo que haya en la pila</td>
+            <td>Desarrollo software</td>
+            <td>Integración IA chachi</td>
+          </tr>
+          <tr>
+            <td>Cosas difíciles</td>
+            <td>Bajar cosas a tierra</td>
+            <td>Cosas puntiagudas</td>
+            <td>Chaquetero de visión</td>
+            <td>Amotinarse y ser Andrés</td>
+            <td>Le hace ojitos a IA</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <section>
+      WAPO WAPO
+      <iframe
+        title="Dashboard"
+        class="r-stretch"
+        src="http://localhost:3000/d-solo/0atAxsnVk/new-dashboard?orgId=1&refresh=5s&panelId=6"
+        width="250"
+        frameborder="0"
+      />
+    </section>
+    <section>
+      WAPO WAPO 2
+      <iframe
+        title="Dashboard"
+        src="http://localhost:3000/d-solo/0atAxsnVk/new-dashboard?orgId=1&refresh=5s&panelId=6"
+        width="450"
+        height="200"
+        frameborder="0"
+      />
+    </section>
+    <section>
+      Slide 3
+      <aside class="notes">
+        <p>Some notes</p>
+      </aside>
+      <p>Some slide text</p>
+      <aside class="notes">
+        <p>and some more notes</p>
+      </aside>
+    </section>
+  </div>
 </div>
+
+<style>
+  div {
+    table-layout: fixed;
+  }
+</style>
