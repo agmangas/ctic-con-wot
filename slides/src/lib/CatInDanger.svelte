647f5716d22bcf89fd7a4dc517189720caf5a12d
Aser Crespo <aser.crespo@fundacionctic.org>
2022-09-30 12:22:40 +0200

    Cat termination added.

--- slides/src/lib/CatInDanger.svelte
+++ slides/src/lib/CatInDanger.svelte
@@ -1,11 +1,14 @@
 <script>
     import catSprite from '/src/assets/cat.png';
     import trainSprite from '/src/assets/moving_train.png';
+    import terminatedCat from '/src/assets/terminated_cat.png'
     import anime from 'animejs/lib/anime.es.js';
     import { onMount } from 'svelte';
 
-    const TIME_TO_DEATH = 61e3;
+    const TIME_TO_DEATH = 5e3;
 
+
+    let catIsDone = false;
     // Binded to elements in the DOM
     let trainWrapperEl;
     let trainEl;
@@ -15,19 +18,21 @@
     let progress = 0;
 
     onMount(()=>{
-        anime({
+        let trainAnimation = anime({
             targets: trainWrapperEl,
             top: "100%",
             translateY: "-100%",
             duration: TIME_TO_DEATH,
             easing: 'linear',
             update: function(anim) {
-                let secondsToDeath = (anim.duration - (anim.duration * anim.progress / 100.0)) / 1e3;
-                let seconds = Math.floor(secondsToDeath % 60);
-                let minutes = Math.floor(secondsToDeath / 60.0);
+                let secondsToTermination = (anim.duration - (anim.duration * anim.progress / 100.0)) / 1e3;
+                let seconds = Math.floor(secondsToTermination % 60);
+                let minutes = Math.floor(secondsToTermination / 60.0);
                 countdown = `${minutes}:${("0" + seconds).slice(-2)}`;
             }
         });
+        trainAnimation.finished.then(() => {catIsDone=true;} );
+
         anime({
             targets: trainEl,
             scaleX: ["1.05","0.9"],
@@ -49,17 +54,24 @@
     
 </script>
 
-<div class="container">
-    <div class="train-track">
-        <p bind:this={catLabelEl} class="cat-label">{countdown}</p>
-        <div bind:this={trainWrapperEl} class="train-wrapper">
-            <img bind:this={trainEl} class="train" src={trainSprite} alt="train drawing"/>
+{#if !catIsDone}
+    <div class="container">
+        <div class="train-track">
+            <p bind:this={catLabelEl} class="cat-label">{countdown}</p>
+            <div bind:this={trainWrapperEl} class="train-wrapper">
+                <img bind:this={trainEl} class="train" src={trainSprite} alt="train drawing"/>
+            </div>
+        </div>
+        <div class="cat-wrapper">
+            <img class="cat" src={catSprite} alt="train drawing"/>
         </div>
     </div>
-    <div class="cat-wrapper">
-        <img class="cat" src={catSprite} alt="train drawing"/>
+{:else}
+    <div class="terminated-cat-container">
+        <img src={terminatedCat} alt="cat terminated"/>
     </div>
-</div>
+{/if}
+
 
 <style>
 
@@ -101,6 +113,14 @@ img {
     width: 80%; 
     height: auto;
     overflow: hidden;
+}
+
+.terminated-cat-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 </style>
     
