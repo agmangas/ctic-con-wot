52afdbd4b10a964d717c21d788266e8cb1dac5c9
Sergio Flórez <sergio.florez@fundacionctic.org>
2022-10-05 12:27:36 +0200

    add sounds emitter static files
    http://localhost:5001/flauta/
    http://localhost:5001/numeros/
    http://localhost:5001/sound-emitter/

--- sound-recognition/src/mqtt-client.js
+++ sound-recognition/src/mqtt-client.js
@@ -1,6 +1,6 @@
 import mqtt from "mqtt/dist/mqtt";
 
-const MQTT_URL = "mqtt://localhost:9001";
+const MQTT_URL = process.env.REACT_APP_MQTT_URL || "mqtt://localhost:9001";
 const DIAPO_ID = 4;
 
 const mqttClient = mqtt.connect(MQTT_URL);
@@ -8,8 +8,12 @@ const TOPIC_NEXT = "slides/command";
 
 function connect() {
     mqttClient.on("connect", () => {
-        console.log("MQTT CLIENT: Connected to MQTT broker");
+        console.log("MQTT CLIENT: Connected to MQTT broker", MQTT_URL);
     });
+}
+
+function disconnect() {
+    mqttClient.end();
 }
 
 function next() {
@@ -27,4 +31,20 @@ function next() {
 
 }
 
-export {connect, next};
+function liveEvent() {
+    if (!mqttClient.connected) {
+        console.log("MQTT CLIENT: Not connected to MQTT broker");
+        return;
+    }
+
+    const infoToSend = {
+        "method": "event",
+        "args": [DIAPO_ID]
+    };
+    console.log("MQTT CLIENT: Sending info:", infoToSend);
+    mqttClient.publish(TOPIC_NEXT, JSON.stringify(infoToSend));
+
+}
+
+
+export { connect, disconnect, next, liveEvent };
